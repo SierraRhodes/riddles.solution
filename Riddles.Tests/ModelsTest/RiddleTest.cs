9079dbf3007f076bfa10b8a1d6d06c8177c30424
SierraRhodes <alreadywrote@gmail.com>
2023-04-16 16:28:45 -0700

    add correct answer functionality + test passed

--- Riddles.Tests/ModelsTest/RiddleTest.cs
+++ Riddles.Tests/ModelsTest/RiddleTest.cs
@@ -28,5 +28,23 @@ public void GameCreation_NewRiddleGame_Void()
     //Assert 
     Assert.IsFalse(string.IsNullOrEmpty(riddle));
   }
+
+   [TestMethod]
+public void CorrectAnswer_UserWins_Void()
+{
+    //Arrange
+    RiddleGame game = new RiddleGame();
+    string riddle = game.GenerateRiddle();
+    string currentCorrectAnswer = game.GetCorrectAnswer(riddle);
+    game.StartGame(currentCorrectAnswer, currentCorrectAnswer);
+
+    //Act
+    bool result = game.CheckAnswer(currentCorrectAnswer);
+
+    //Assert
+    Assert.IsTrue(result);
+    Assert.IsTrue(game.UserHasWon);
+    Assert.IsTrue(game.IsGameOver);
+}
   }
 }
